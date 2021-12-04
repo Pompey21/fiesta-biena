94179d9a50335f197f43494c84ab426ec3364580
Marko Mekjavic <s1813308@caritoux.inf.ed.ac.uk>
2021-12-04 17:26:17 +0000

    getting to know classes in python

--- EVAL.py
+++ EVAL.py
@@ -3,8 +3,111 @@ import math
 
 class Eval:
     def __init__(self,file_qrels,file_sys_res):
-        self.file_qrels = file_qrels
-        self.file_sys_res = file_sys_res
+        self.file_qrels = self.create_dict_qrels(file_qrels)
+        self.file_sys_res = self.create_dict_sys_res(file_sys_res)
+        self.p_10 = self.p_10(self.file_sys_res)
+        self.r_50 = self.r_50(self.file_sys_res)
+
+    # def __call__(self, *args, **kwargs):
+
+    def create_dict_qrels(self,file_qrels_file):
+        # we have 10 queries
+        # which documents are relevant to which query
+        file_qrels = open(file_qrels_file)
+        csvreader_qrels = csv.reader(file_qrels)
+        rows_qrels = [row for row in csvreader_qrels]
+        rows_qrels = rows_qrels[1:]
+
+        # KEYS : Number of Query
+        # VALUES : Relevant documents for each query
+        qrels_dict = {}
+        for elem in rows_qrels:
+            if elem[0] not in qrels_dict.keys():
+                qrels_dict[elem[0]] = [(elem[1], elem[2])]
+            else:
+                qrels_dict.get(elem[0]).append((elem[1], elem[2]))
+        return qrels_dict
+    def create_dict_sys_res(self,file_sys_res_file):
+        # we have 6 systems
+        # which documents were retrieved by which system
+        file_system_results = open(file_sys_res_file)
+        csvreader_system_results = csv.reader(file_system_results)
+        rows_system_results = [row for row in csvreader_system_results]
+        rows_system_results = rows_system_results[1:]
+
+        # create dict
+        # KEYS : Number of System
+        # VALUES : (query_number,doc_number,rank_of_doc,score)
+        system_results_dict = {}
+        for elem in rows_system_results:
+            if elem[0] not in system_results_dict.keys():
+                system_results_dict[elem[0]] = [(elem[1], elem[2], elem[3], elem[4])]
+            else:
+                # qrels_dict[elem[0]] = qrels_dict.get(elem[0]).append((elem[1],elem[2]))
+                system_results_dict.get(elem[0]).append((elem[1], elem[2], elem[3], elem[4]))
+
+        # create dict
+        # KEYS_1 : Number of System
+        # KEYS_2 : Number of Query
+        # VALUES : Documents (doc_number,rank_of_doc,score)
+        for system in system_results_dict.keys():
+            query_dict = {}
+            for query in system_results_dict.get(system):
+                if query[0] not in query_dict.keys():
+                    query_dict[query[0]] = [(query[1], query[2], query[3])]
+                else:
+                    query_dict.get(query[0]).append((query[1], query[2], query[3]))
+            system_results_dict[system] = query_dict
+
+        return system_results_dict
+
+    # P@10
+    # consider only 10 elements for each query for each system
+    def p_10(self,system_results_dict):
+        system_queries_precision = []
+        for system in system_results_dict.keys():
+            queries_precission = []
+            for query in system_results_dict.get(system).keys():
+                precision_counter = 0
+                for doc in system_results_dict.get(system).get(query)[:10]:
+                    if doc[0] in self.tuples_first(qrels_dict.get(query)):
+                        precision_counter = precision_counter + 1
+                query_precission = (query, precision_counter / 10)
+                queries_precission.append(query_precission)
+            system_queries_precision.append(queries_precission)
+        return system_queries_precision
+
+    # R@50
+    # consider only 50 elements for each query for each system
+    def r_50(self,system_results_dict):
+        system_queries_recall = []
+        for system in system_results_dict.keys():
+            queries_recall = []
+            for query in system_results_dict.get(system).keys():
+                # system_results_dict.get(system)[query] = system_results_dict.get(system).get(query)[:10]
+                recall_counter = 0
+                for doc in system_results_dict.get(system).get(query)[:50]:
+                    if doc[0] in tuples_first(qrels_dict.get(query)):
+                        recall_counter = recall_counter + 1
+                query_recall = (query, recall_counter / len(tuples_first(qrels_dict.get(query))))
+                # print(query_recall)
+                queries_recall.append(query_recall)
+            system_queries_recall.append(queries_recall)
+        return system_queries_recall
+
+    # HELPER FUNCTIONS
+    # returns the first element of a tuple : (a,b) -> a
+    def tuples_first(self,lst_tuples):
+        return [tpl[0] for tpl in lst_tuples]
+    def tuple_second(self,lst_tuples):
+        return [tpl[1] for tpl in lst_tuples]
+    def fill_list(self,lst, size):
+        for i in range(size - len(lst)):
+            lst.append(0)
+        return lst
+
+
+
 
 
 # we have 10 queries
@@ -313,45 +416,46 @@ def nDCG_20():
 # system_queries_DG = nDCG_20()
 # print(system_queries_DG)
 
+eval = Eval('qrels.csv','system_results.csv')
+print(eval.p_10)
 
-
-p_10_var = p_10()
-r_50_var = r_50()
-r_precission_var = r_precission()
-average_precission_var = average_precission()
-nDCG_10_var = nDCG_10()
-nDCG_20_var = nDCG_20()
-
-
-
-# system_results_dict
-# Generate output!
-output = open("ir_eval.csv", "w+")
-for count_1,system in enumerate(system_results_dict):
-    system_output = []
-    p_10_mean= 0
-    r_50_mean = 0
-    r_precission_mean = 0
-    ap_mean = 0
-    ndcg10_mean = 0
-    ndcg20_mean = 0
-    for count_2, query in enumerate(qrels_dict):
-        query_output = [str(count_1+1),str(count_2+1),str(round(p_10_var[count_1][count_2][1],3)),str(round(r_50_var[count_1][count_2][1],3)),
-                        str(round(r_precission_var[count_1][count_2][1],3)),str(round(average_precission_var[count_1][count_2],3)),
-                        str(round(nDCG_10_var[count_1][count_2],1)),str(round(nDCG_20_var[count_1][count_2],3))]
-        p_10_mean = p_10_mean + p_10_var[count_1][count_2][1]
-        r_50_mean = r_50_mean + r_50_var[count_1][count_2][1]
-        r_precission_mean = r_precission_mean + r_precission_var[count_1][count_2][1]
-        ap_mean = ap_mean + average_precission_var[count_1][count_2]
-        ndcg10_mean = ndcg10_mean + nDCG_10_var[count_1][count_2]
-        ndcg20_mean = ndcg20_mean + nDCG_20_var[count_1][count_2]
-
-        query_output_str = ','.join(query_output)
-        output.write(query_output_str + '\n')
-    query_mean_output = [str(count_1+1),'mean',str(round(p_10_mean/(count_2+1),3)),str(round(r_50_mean/(count_2+1),3)),str(round(r_precission_mean/(count_2+1),3)),
-                         str(round(ap_mean/(count_2+1),3)),str(round(ndcg10_mean/(count_2+1),3)),str(round(ndcg20_mean/(count_2+1),3))]
-    query_mean_output_str = ','.join(query_mean_output)
-    output.write(query_mean_output_str + '\n')
+# p_10_var = p_10()
+# r_50_var = r_50()
+# r_precission_var = r_precission()
+# average_precission_var = average_precission()
+# nDCG_10_var = nDCG_10()
+# nDCG_20_var = nDCG_20()
+#
+#
+#
+# # system_results_dict
+# # Generate output!
+# output = open("ir_eval.csv", "w+")
+# for count_1,system in enumerate(system_results_dict):
+#     system_output = []
+#     p_10_mean= 0
+#     r_50_mean = 0
+#     r_precission_mean = 0
+#     ap_mean = 0
+#     ndcg10_mean = 0
+#     ndcg20_mean = 0
+#     for count_2, query in enumerate(qrels_dict):
+#         query_output = [str(count_1+1),str(count_2+1),str(round(p_10_var[count_1][count_2][1],3)),str(round(r_50_var[count_1][count_2][1],3)),
+#                         str(round(r_precission_var[count_1][count_2][1],3)),str(round(average_precission_var[count_1][count_2],3)),
+#                         str(round(nDCG_10_var[count_1][count_2],1)),str(round(nDCG_20_var[count_1][count_2],3))]
+#         p_10_mean = p_10_mean + p_10_var[count_1][count_2][1]
+#         r_50_mean = r_50_mean + r_50_var[count_1][count_2][1]
+#         r_precission_mean = r_precission_mean + r_precission_var[count_1][count_2][1]
+#         ap_mean = ap_mean + average_precission_var[count_1][count_2]
+#         ndcg10_mean = ndcg10_mean + nDCG_10_var[count_1][count_2]
+#         ndcg20_mean = ndcg20_mean + nDCG_20_var[count_1][count_2]
+#
+#         query_output_str = ','.join(query_output)
+#         output.write(query_output_str + '\n')
+#     query_mean_output = [str(count_1+1),'mean',str(round(p_10_mean/(count_2+1),3)),str(round(r_50_mean/(count_2+1),3)),str(round(r_precission_mean/(count_2+1),3)),
+#                          str(round(ap_mean/(count_2+1),3)),str(round(ndcg10_mean/(count_2+1),3)),str(round(ndcg20_mean/(count_2+1),3))]
+#     query_mean_output_str = ','.join(query_mean_output)
+#     output.write(query_mean_output_str + '\n')
 
 
 
